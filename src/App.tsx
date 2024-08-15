69e20369c7ffdb46f38f3d8a7c2a9e6e3b626d3a
david-grig-developer <david.grig.developer@gmail.com>
2024-08-15 23:57:11 +0400

    feat: add Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./Main";
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Main />
+  </div>;
 }
 
 export default App;
